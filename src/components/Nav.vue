384679f7214f7e846c2244fec949219a8db49200
Chloe <chloea@hotmail.fr>
2023-07-10 19:08:40 +0200

    progress

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,57 +1,32 @@
 <template>
   <nav>
-    <!-- <PersonalRouter :route="route" :buttonText="buttonText" class="logo-link"/> -->
-    <router-link to="/">
-      Home
-    </router-link>
-
-    <ul>
-        <li>
-          <router-link to="/">Task Manager</router-link>
-        </li>
-
-        <li>
-          <router-link to="/account">Your Account</router-link>
-        </li>
-    </ul>
-
-    <div>
-      <ul>
-        <li class="log-out-welcome">
-          <p>Welcome, user</p>
-        </li>
-      </ul>
-    </div>
+    <!-- <router-link to="/">Home</router-link> | -->
+    <!-- <router-link to="SignIn">Sign In</router-link> | -->
+    <!-- <router-link to="SignUp>Sign Up</router-link> | -->
+    <router-link to="/">Task Manager</router-link>
+    <button @click="logout">Log Out</button>
   </nav>
+  <router-view/>
 </template>
 
 <script setup>
-// import PersonalRouter from "./PersonalRouter.vue";
-import { useUserStore } from "../stores/user";
-import { computed } from "vue";
 import { useRouter } from "vue-router";
+import { useUserStore } from "../stores/user";
 import { ref } from 'vue';
+import { supabase } from "../supabase";
 
-//constant to save a variable that will hold the use router method
-const route = "/";
-const buttonText = "Todo app";
-
-// constant to save a variable that will get the user from store with a computed function imported from vue
-// const getUser = computed(() => useUserStore().user);
+const router = useRouter();
 const getUser = useUserStore().user;
+const userEmail = ref(getUser.email);
 
-// constant that calls user email from the useUSerStore
-const userEmail = getUser.email;
-
-// async function that calls the signOut method from the useUserStore and pushes the user back to the Auth view.
-const redirect = useRouter();
-
-const signOut = async () => {
-  try{
-    // call the user store and send the users info to backend to signOut
-    // then redirect user to the homeView
-  } catch (error) {}
-};
+async function logout() {
+  try {
+    await supabase.auth.signOut();
+    router.push('/auth');
+  } catch (error) {
+    console.log(error);
+  }
+}
 </script>
 
 <style scoped>
